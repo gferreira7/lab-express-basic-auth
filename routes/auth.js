b465ceddb21f493880d8021a1562820159863bbc
gferreira7 <104030930+gferreira7@users.noreply.github.com>
2023-02-27 18:37:09 +0000

    finishing it1

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,47 @@
-const { Router } = require('express');
-const router = new Router();
+const { Router } = require('express')
+const router = new Router()
+
+const mongoose = require('mongoose') // <== has to be added
+
+const bcryptjs = require('bcryptjs')
+let saltRounds = 10
+
+const User = require('../models/User.model')
 
 router.get('/signup', (req, res, next) => {
   res.render('auth/signup')
 })
 
+router.post('/signup', (req, res, next) => {
+
+  const { username, password } = req.body
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        username,
+        password: hashedPassword,
+      })
+    })
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.redirect('/')
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message })
+      } else if (error.code === 11000) {
+        res.status(500).render('auth/signup', {
+          errorMessage:
+            'Username needs to be unique. Username is already used.',
+        })
+      } else {
+        next(error)
+      }
+    })
+  
+
+})
 module.exports = router
